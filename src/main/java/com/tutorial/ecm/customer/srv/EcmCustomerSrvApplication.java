9cdab16c3f43fd4deec54872a9c03d2d44e96535
The Flying Gibbon <73854239+the-flying-gibbon@users.noreply.github.com>
2024-07-01 17:29:15 +0200

    Add files via upload

---
+++ src/main/java/com/tutorial/ecm/customer/srv/EcmCustomerSrvApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.ecm.customer.srv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcmCustomerSrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcmCustomerSrvApplication.class, args);
+	}
+
+}
